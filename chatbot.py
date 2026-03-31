f3e59c8f8b38b98b22d84be4a3b88b22fb7c5f42
revelation-ai <weidemancorne8@gmail.com>
2026-03-31 10:15:34 +0200

    Fix create_stuff_documents_chain import for langchain-classic

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import AIMessage, HumanMessage
 
